9807743e252dd108cf0b6b128ec42151afefb1e3
Drew <kevinfunderburkbusiness@gmail.com>
2020-12-11 11:32:13 -0600

    Added replay game. Checked for wins/cat games. Removed duplicate code

--- TicTacToe/Main.cpp
+++ TicTacToe/Main.cpp
@@ -6,32 +6,24 @@ char player2Name[64];
 bool player1Victory = false;
 bool player2Victory = false;
 
+// Set a player's name
 void setName(int index)
 {
-	// Loop until valid input is given
-	while (true)
+	if (index == 1)
 	{
-		if (index == 1)
-		{
-			std::cout << "Please enter a name for Player 1: ";
-			std::cin >> player1Name;
-			std::cout << "Welcome " << player1Name << "!\n";
-			break;
-		}
-		else if (index == 2)
-		{
-			std::cout << "Please enter a name for Player 2: ";
-			std::cin >> player2Name;
-			std::cout << "Welcome " << player2Name << "!\n";
-			break;
-		}
-		else
-		{
-			std::cout << "Invalid entry.\n";
-		}
+		std::cout << "Please enter a name for Player 1: ";
+		std::cin >> player1Name;
+		std::cout << "Welcome " << player1Name << "!\n";
+	}
+	else if (index == 2)
+	{
+		std::cout << "Please enter a name for Player 2: ";
+		std::cin >> player2Name;
+		std::cout << "Welcome " << player2Name << "!\n";
 	}
 }
 
+// Draw the TicTacToe grid to the console
 void drawGrid()
 {
 	char charGrid[3][3];
@@ -76,93 +68,60 @@ void drawGrid()
 	}
 }
 
-int getInput()
+// Cin an integer and return it
+int getIntegerInput()
 {
 	int input;
 	std::cin >> input;
 	return input;
 }
 
+// Get a grid position from a player and set it to their char
 void setGrid(int index)
 {
 	if (index == 1)
-	{
 		std::cout << player1Name << "! It's your turn! [X]\n";
-		int x = 0;
-		int y = 0;
-		while (true)
-		{
-			while (x == 0)
-			{
-				std::cout << " Enter a row: ";
-				int input = getInput();
-				if (input > 0 && input < 4)
-					x = input;
-				else
-					continue;
-			}
-			while (y == 0)
-			{
-				std::cout << " Enter a column: ";
-				int input = getInput();
-				if (input > 0 && input < 4)
-					y = input;
-				else
-					continue;
-			}
-			if (positions[x - 1][y - 1] == 0)
-			{
-				positions[x - 1][y - 1] = 1;
-				break;
-			}
-			else
-			{
-				std::cout << "That position is taken!\n\n";
-				x = 0;
-				y = 0;
-			}
-		}
-	}
 	else if (index == 2)
-	{
 		std::cout << player2Name << "! It's your turn! [O]\n";
-		int x = 0;
-		int y = 0;
-		while (true)
+
+	int x = 0;
+	int y = 0;
+	// Loop until valid input
+	while (true)
+	{
+		while (x == 0)
 		{
-			while (x == 0)
-			{
-				std::cout << " Enter a row: ";
-				int input = getInput();
-				if (input > 0 && input < 4)
-					x = input;
-				else
-					continue;
-			}
-			while (y == 0)
-			{
-				std::cout << " Enter a column: ";
-				int input = getInput();
-				if (input > 0 && input < 4)
-					y = input;
-				else
-					continue;
-			}
-			if (positions[x - 1][y - 1] == 0)
-			{
-				positions[x - 1][y - 1] = 2;
-				break;
-			}
+			std::cout << " Enter a row: ";
+			int input = getIntegerInput();
+			if (input > 0 && input < 4)
+				x = input;
 			else
-			{
-				std::cout << "That position is taken!\n\n";
-				x = 0;
-				y = 0;
-			}
+				continue;
+		}
+		while (y == 0)
+		{
+			std::cout << " Enter a column: ";
+			int input = getIntegerInput();
+			if (input > 0 && input < 4)
+				y = input;
+			else
+				continue;
+		}
+		if (positions[x - 1][y - 1] == 0)
+		{
+			positions[x - 1][y - 1] = index;
+			break;
+		}
+		else
+		{
+			std::cout << "That position is taken!\n\n";
+			x = 0;
+			y = 0;
 		}
 	}
 }
 
+// Check for 3 in a row or cat game
 bool checkWin()
 {
 	for (int i = 0; i < 3; i++)
@@ -193,6 +152,7 @@ bool checkWin()
 			positions[i][2] == 2)
 			return player2Victory = true;
 	}
+
 	// Player 1
 	// Diag 1
 	if (positions[0][0] == 1 &&
@@ -217,56 +177,94 @@ bool checkWin()
 		positions[0][2] == 2)
 		return player2Victory = true;
 
+	// If all victory checks fail and there are no open spaces, cat game
+	bool cat = true;
+	for (int i = 0; i < 3; i++)
+		for (int n = 0; n < 3; n++)
+			if (positions[i][n] == 0)
+				cat = false;
+
+	if (cat)
+	{
+		player1Victory = true;
+		player2Victory = true;
+		return true;
+	}
+
 	return false;
 }
 
 int main()
 {
-	// Get names
-	setName(1);
-	setName(2);
-	std::cout << std::endl;
-	system("pause");
-	system("cls");
-
-	// Init all positions to 0
-	for (int i = 0; i < 3; i++)
+	while (true)
 	{
-		for (int n = 0; n < 3; n++)
+		system("cls");
+		// Get names
+		setName(1);
+		setName(2);
+		std::cout << std::endl;
+		system("pause");
+		system("cls");
+
+		// Init all positions to 0
+		for (int i = 0; i < 3; i++)
 		{
-			positions[i][n] = 0;
+			for (int n = 0; n < 3; n++)
+			{
+				positions[i][n] = 0;
+			}
 		}
-	}
-	
-	// Game Loop
-	while (!player1Victory && !player2Victory)
-	{
+
+		// Set victories false
+		player1Victory = false;
+		player2Victory = false;
+
+		// Game Loop
+		while (!player1Victory && !player2Victory)
+		{
+			system("cls");
+			drawGrid();
+			std::cout << std::endl;
+			setGrid(1);
+			system("cls");
+			drawGrid();
+			std::cout << std::endl;
+			if (checkWin())
+				break;
+			setGrid(2);
+			checkWin();
+		}
+
 		system("cls");
 		drawGrid();
-		std::cout << std::endl;
-		setGrid(1);
-		system("cls");
-		drawGrid();
-		std::cout << std::endl;
-		if (checkWin())
+
+		// Victory messages
+		if (player1Victory && !player2Victory)
+		{
+			std::cout << std::endl;
+			std::cout << player1Name << " wins!\n\n";
+		}
+		else if (player2Victory && !player1Victory)
+		{
+			std::cout << std::endl;
+			std::cout << player2Name << " wins!\n\n";
+		}
+		else if (player2Victory && player1Victory)
+		{
+			std::cout << std::endl;
+			std::cout << "Cat!\n\n";
+		}
+
+		// Check for play again
+		std::cout << "Would you like to play again?\n";
+		std::cout << " [1] Yes\n";
+		std::cout << " [2] No\n";
+		std::cout << "> ";
+
+		int input = getIntegerInput();
+		if (input == 2)
 			break;
-		setGrid(2);
-		checkWin();
 	}
 
-	system("cls");
-	drawGrid();
-
-	if (player1Victory)
-	{
-		std::cout << std::endl;
-		std::cout << player1Name << " wins!\n\n";
-	}
-	else if (player2Victory)
-	{
-		std::cout << std::endl;
-		std::cout << player2Name << " wins!\n\n";
-	}
-	system("pause");
 	return 0;
 }
