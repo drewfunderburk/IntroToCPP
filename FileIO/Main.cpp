ae3b8226acef7b91c015d36f46050d86cf523f21
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2021-02-05 08:32:24 -0600

    Commented out unused code in FileIO to make BinaryFile less hidden

--- FileIO/Main.cpp
+++ FileIO/Main.cpp
@@ -2,7 +2,12 @@
 #include <fstream>
 #include "Character.h"
 
-int main()
+/*
+* 
+* This is all just keeping the two save systems separate so I can use them later
+* Binary File IO is in main
+* 
+void textFileIO()
 {
 
 	Character player = Character();
@@ -33,6 +38,85 @@ int main()
 
 	std::cout << player2.health << std::endl;
 	std::cout << player2.damage << std::endl;
+}
+
+void binaryFileIO()
+{
+	Character sbeve = Character();
+	sbeve.health = 200;
+	sbeve.damage = 10;
+
+	Character sbeve2 = Character();
+	sbeve2.health = 5;
+	sbeve2.damage = 3;
+
+	Character sbeve3 = Character();
+	sbeve3.health = 1000;
+	sbeve3.damage = 40;
+
+	std::fstream file;
+	file.open("data.dat", std::ios::out | std::ios::binary);
+	if (file.is_open())
+	{
+		file.write((char*)&sbeve, sizeof(Character));
+		file.write((char*)&sbeve2, sizeof(Character));
+		file.write((char*)&sbeve3, sizeof(Character));
+	}
+	file.close();
+
+
+	Character covfefe = Character();
+
+	file.open("data.dat", std::ios::in | std::ios::binary);
+	if (file.is_open())
+	{
+		file.seekg(sizeof(Character) * 2, std::ios::beg);
+		file.read((char*)&covfefe, sizeof(Character));
+	}
+	file.close();
+
+	std::cout << covfefe.health << std::endl;
+	std::cout << covfefe.damage << std::endl;
+
+}
+*/
+
+int main()
+{
+	std::fstream file;
+	Character sbeve = Character();
+	file.open("save.dat", std::ios::in | std::ios::binary);
+	if (file.is_open())
+	{
+		int input;
+		std::cout << "Which character would you like to load?" << std::endl;
+		std::cout << "> ";
+		std::cin >> input;
+
+		file.seekg(sizeof(Character) * input, std::ios::beg);
+		file.read((char*)&sbeve, sizeof(Character));
+	}
+	else
+		std::cout << "No save file found. Will create one this run." << std::endl;
+	file.close();
+
+	std::cout << sbeve.health << std::endl;
+	std::cout << sbeve.damage << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "What stats would you like to have?" << std::endl;
+	std::cout << "Health: ";
+	std::cin >> sbeve.health;
+	std::cout << "Damage:";
+	std::cin >> sbeve.damage;
+
+	file.open("save.dat", std::ios::out | std::ios::app | std::ios::binary);
+	if (!file.is_open())
+		return 1;
+
+	file.write((char*)&sbeve, sizeof(Character));
+	file.close();
+
 
 	return 0;
 }
